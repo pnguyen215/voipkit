9d4fa4ecba9f5598e6ebff262f625d440503234b
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-03-12 22:22:34 +0700

    :ok_hand: chore: added field date_recevied_at on each event #42

--- pkg/ami/config/ami_field.go
+++ pkg/ami/config/ami_field.go
@@ -1,12 +1,12 @@
 package config
 
 const (
-	AmiFieldContext   = "Context"
-	AmiFieldChannel   = "Channel"
-	AmiFieldExtension = "Exten"
-	AmiFieldPriority  = "Priority"
-	AmiFieldTimeout   = "Timeout"
-	AmiFieldVariable  = "Variable"
-	AmiFieldAsync     = "Async"
-	AmiFieldDateReceivedAt = "DateReceivedAt"
+	AmiFieldContext        = "Context"
+	AmiFieldChannel        = "Channel"
+	AmiFieldExtension      = "Exten"
+	AmiFieldPriority       = "Priority"
+	AmiFieldTimeout        = "Timeout"
+	AmiFieldVariable       = "Variable"
+	AmiFieldAsync          = "Async"
+	AmiFieldDateReceivedAt = "Datereceivedat"
 )
